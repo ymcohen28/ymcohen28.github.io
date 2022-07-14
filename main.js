d1b83a9d17a932d5c978a20667c190fab6490f22
YMC <109100452+ymcohen@users.noreply.github.com>
2022-07-14 22:12:35 +0100

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,4 @@ var setTime = function(){
   label.textContent = audio1.currentTime
 };
 setInterval(setTime, 1);
-var header = document.getElementsByClassName("head");
-for (var i = 0; i < header.length; i++) {
-header[i].textContent = 'hacked'
-}
+
