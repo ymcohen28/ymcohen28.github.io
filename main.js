da7cc01e6d39d1410ade0b882aad3e18e35d2964
YMC <109100452+ymcohen@users.noreply.github.com>
2022-07-15 18:18:27 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-var audio = document.getElementById("audio1");
+var audio1 = document.getElementById("audio1");
 var label = document.getElementById("label");
-var a = function() {label.textContent += audio.currentTime
-                   label.textContent+= audio.src
+var a = function() {
+  label.textContent = audio1.currentTime
+                   label.textContent+= audio1.src
                    };
 setInterval(a,1);
