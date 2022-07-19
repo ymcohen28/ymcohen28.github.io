ce55453dd87cfde6045d790b1e944c8d86275b24
YMC <109100452+ymcohen@users.noreply.github.com>
2022-07-19 19:57:40 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 var audio1 = document.getElementById("audio1");
 var label = document.getElementById("label");
+setInterval(an,1);
 var an = function() {
   label.textContent = audio1.currentTime;
   label.textContent+= audio1.src;
   audio1.currentTime = 2; 
 };
-setInterval(an,1);
+
 var header = document.getElementsByClassName("head");
 for (var i = 0; i < header.length; i++) {
-header[i].textContent = 'hacked';
+header[i].textContent = 'This is Hacked!!';
 }
