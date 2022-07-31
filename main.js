66f3cd60a401260a86839ce7084500f1ff708bb9
YMC <109100452+ymcohen@users.noreply.github.com>
2022-07-31 19:42:41 +0100

    Update main.js

--- main.js
+++ main.js
@@ -6,16 +6,15 @@ var an = function() {
   label.textContent+= audio1.src;
   audio1.currentTime = 2; 
 };
-var logo = document.getElementsByClassName("logo");
-for (var i = 0; i < logo.length; i++) {
-  if (window.innerWidth > 400) {logo.width = window.innerWidth / 10;}
-  else {logo.width = window.innerHeight / 10;}
-}
-
 var text = "<div class='header'><a style='float:left; position:fixed;' href='/index.html'><img class='logo' title = 'Site Logo - Home Page' alt = 'Site Logo' id='logo-header' src='/logo.png' ></a><table style='padding-right:100px; float:right;'><tr><td><a class='head' href='/index.html'>Home</a></td><td><a class='head' href='/blog.html'>Blog</a></td><td><a class='head' href='/music.html'>My Music</a></td><td><a class='head' title = 'Note: This is for testing purposes only and will PROBABLY contain more bugs than the rest of the site!!' href='/test.html'>Test</a></td></tr></table></div>"
 var realHeader = document.getElementsByTagName("header");
 for (var i = 0; i < realHeader.length;i ++) {  
 realHeader[i].innerHTML = text;}  
 var header = document.getElementsByClassName("head");
 for (var i = 0; i < header.length; i++) {
+}
+var logo = document.getElementsByClassName("logo");
+for (var i = 0; i < logo.length; i++) {
+  if (window.innerWidth > 400) {logo.width = window.innerWidth / 10;}
+  else {logo.width = window.innerHeight / 10;}
 }
