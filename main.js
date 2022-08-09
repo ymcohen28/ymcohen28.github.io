22448cba3a5f1981225e169634ca35e8294ade2a
YMC <109100452+ymcohen28@users.noreply.github.com>
2022-08-09 19:15:16 +0100

    Update main.js

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ setInterval(an,1);
 var an = function() {
   label.textContent = audio1.currentTime;
   label.textContent+= audio1.src;
+  label.textContent += window.outerHeight;
+  label.textContent += window.outerWidth;
 };
 var text = "<div class='header'><a style='float:left; position:fixed;' href='/index.html'><img class='logo' title = 'Site Logo - Home Page' alt = 'Site Logo' id='logo-header' src='/logo.png' ></a><table style='padding-right:100px; float:right;'><tr><td><a class='head' href='/index.html'>Home</a></td><td><a class='head' href='/blog.html'>Blog</a></td><td><a class='head' href='/music.html'>My Music</a></td><td><a class='head' title = 'Note: This is for testing purposes only and will PROBABLY contain more bugs than the rest of the site!!' href='/test.html'>Test</a></td></tr></table></div>"
 var realHeader = document.getElementsByTagName("header");
