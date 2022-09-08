38bc39b2d6f0879fd188a2d66ea349edb937f1cc
YMC <109100452+ymcohen28@users.noreply.github.com>
2022-09-08 15:26:42 +0100

    Update main.js

--- main.js
+++ main.js
@@ -7,16 +7,19 @@ var an = function() {
   label.textContent += window.outerHeight;
   label.textContent += window.outerWidth;
 };
-var text = "<div class='header'><a href='/index.html'><img class='logo' title = 'Site Logo - Home Page' alt = 'Site Logo' id='logo-header' src='/logo.png' ></a><table style=' float:right;'><tr><td><a class='head' href='/index.html'>Home</a></td><td><a class='head' href='/blog.html'>Blog</a></td><td><a class='head' href='/music.html'>My Music</a></td><td><a class='head' title = 'Note: This is for testing purposes only and will PROBABLY contain more bugs than the rest of the site!!' href='/test.html'>Test</a></td></tr></table></div>"
-var realHeader = document.getElementsByTagName("header");
-for (var i = 0; i < realHeader.length;i ++) {  
-realHeader[i].innerHTML = text;}  
+
+var text = "<div class='header'><a href='/index.html'><img class='logo' title = 'Site Logo - Home Page' alt = 'Site Logo' id='logo-header' src='/logo.png' ></a><table style=' float:right;'><tr><td><a class='head' href='/index.html'>Home</a></td><td><a class='head' href='/blog.html'>Blog</a></td><td><a class='head' href='/music.html'>My Music</a></td><td><a class='head' title = 'Note: This is for testing purposes only and will PROBABLY contain more bugs than the rest of the site!!' href='/test.html'>Test</a></td></tr></table></div>";
+
 var header = document.getElementsByClassName("head");
 for (var i = 0; i < header.length; i++) {
 }
 var head = document.getElementsByClassName("head");
 for (var i = 0; i < head.length; i++) {
   if (Window.outerWidth > 400) {head[i].style.fontSize = "20px"}
-  else {head[i].style.fontSize = "10px";}
+  else {text = "<img id = "smartPhoneNav" src = "/logo.png"></img>;}
 }
 
+
+var realHeader = document.getElementsByTagName("header");
+for (var i = 0; i < realHeader.length;i ++) {  
+realHeader[i].innerHTML = text;}  
